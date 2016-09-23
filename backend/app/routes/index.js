cd8610a0b9339597c81aefef11b9a3fe399b1f49
Jaime Clinton <jaime.clinton3@gmail.com>
2016-09-24 00:10:19 +0100

    added some get stuff for quynh

--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -10,5 +10,8 @@ router.get('/yo', function(req, res, next) {
    res.render('magic', { title: 'hey', magicmagic: 'this is true magic' });
 });
 
+router.get('/quynh', function(req, res, next) {
+   res.send({ alcohol: 'Beer', price: '5.70', currency: 'GBP'});
+});
 
 module.exports = router;
